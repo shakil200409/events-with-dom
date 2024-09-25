50537c7da98a4a2068390a32c06caa5873ec3022
Shakil Ahmed <shakil200409@gmail.com>
2024-09-25 22:19:23 +0600

    Tried onclick event

---
+++ event.js
@@ -0,0 +1,22 @@
+console.log('This is Event with DOM');
+
+// Changing bg with function (system 2)
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+
+// Changing bg with id on function(System 3)
+const blueBg = document.getElementById('blue-bg');
+blueBg.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+// Simple way of (System 3)
+const purpleBg = document.getElementById('purple-bg');
+purpleBg.onclick = function (){
+    document.body.style.backgroundColor = 'purple';
+}
